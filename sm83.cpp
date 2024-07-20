2ad736e9d062d110d3b0b5826d40925f43bd58a7
org <ogamespec@gmail.com>
2024-07-20 23:51:11 +0300

    Update sm83.cpp

--- sm83.cpp
+++ sm83.cpp
@@ -33,30 +33,27 @@ static uint8_t swap_t[256];			// Preswapped values
 *******
 */
 
-/* _op_ paremeter must be "+=" for DAA and "-=" for DAS operation (determined by NF flag)
-*/
-#define _DAA(_op_) {\
+#define _DAA() {\
 		if((tmp32&0xF)>9 || R_F&HF) {\
-			tmp32 _op_ 6;\
-			tmp8|=(uint8_t)HF;\
+			tmp32 += 6;\
 		}\
-		if(tmp32 > 0x99) {\
-			tmp32 _op_ 0x60;\
-			tmp8|=(uint8_t)CF;\
-}		}
+		if(tmp32 > 0x9F || R_F&CF) {\
+			tmp32 += 0x60;\
+		}\
+}
 
-#define _DAS(_op_) {\
+#define _DAS() {\
 		if(R_F&HF) {\
-			tmp32 _op_ 6;\
-			if((signed)tmp32<0) tmp8&=CF;\
+			tmp32 = (tmp32 - 6) & 0xff;\
 		}\
-		if(tmp8&CF)\
-			tmp32 _op_ 0x60;\
+		if(tmp8&CF) {\
+			tmp32 -= 0x60;\
+		}\
 }
 
 #define DAA(x) {\
-	tmp32=(unsigned)R_A+((unsigned)((R_F&CF)?1:0)<<8);\
-	if(tmp8=(R_F&NF)) _DAS(-=) else _DAA(+=)\
+	tmp32=(unsigned)R_A;\
+	if(tmp8=(R_F&NF)) _DAS() else _DAA();\
 	R_A = (uint8_t)tmp32;\
 	R_F = tmp8 | zr_t[R_A];\
 }
@@ -71,6 +68,7 @@ static uint8_t swap_t[256];			// Preswapped values
 #define POPAF  { r_af.l=RD(R_SP)&(ZF|NF|HF|CF); R_SP++; r_af.h=RD(R_SP); R_SP++; }
 
 
+// TODO: Look in the ALU circuitry to see what the hell is going on here
 #define ADDSPX(n)  \
 	tmp32 = (unsigned)R_SP + (int16_t)(int8_t)n;\
 	R_F = ((R_SP & 0xf) + ((int16_t)(int8_t)n & 0xf)) > 0xf ? HF : 0;\
